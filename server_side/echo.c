1bd394a0a4b32b801b5f0b01a99aaa57fef9f843
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-03-26 16:32:03 +0100

    Comment echo.c

--- server_side/echo.c
+++ server_side/echo.c
@@ -5,51 +5,56 @@
 
 void echo(int connfd)
 {
-    size_t n;
-    char buf[MAXLINE];
-    rio_t rio;
+  size_t n;
+  char buf[MAXLINE];
+  rio_t rio;
 
-    Rio_readinitb(&rio, connfd);
-    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+  // Initializes a read buffer for the descriptor connfd
+  Rio_readinitb(&rio, connfd);
+  // Reads a line from the descriptor connfd (The clients request)
+  while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+  {
+    // If the client wants to get a file
+    if (strncmp(buf, "get ", 4) == 0)
     {
-        if (strncmp(buf, "get ", 4) == 0)
+      char filename[MAXLINE];
+      sscanf(buf + 4, "%s", filename);
+      printf("Server received request for file %s\n", filename);
+
+      FILE *file = Fopen(filename, "rb");
+
+      if (file != NULL)
+      { // If file exists and can be opened
+        char filebuf[MAXLINE];
+        ssize_t bytes_read;
+
+        fseek(file, 0, SEEK_END);                     // Seek to the end of the file
+        long file_size = ftell(file);                 // Get the current file pointer
+        rewind(file);                                 // Seek back to the beginning of the file
+        rio_writen(connfd, &file_size, sizeof(long)); // Send the size of the file
+
+        // Read the file
+        while ((bytes_read = Fread(filebuf, 1, MAXLINE, file)) > 0)
         {
-            char filename[MAXLINE];
-            sscanf(buf + 4, "%s", filename); 
-            printf("Server received request for file %s\n", filename);
+          printf("Line: %s\n", filebuf);
 
-            FILE *file = Fopen(filename, "rb");
-            if (file != NULL)
-            {
-                char filebuf[MAXLINE];
-                ssize_t bytes_read;
-                
-                fseek(file, 0, SEEK_END);
-                long file_size = ftell(file);
-                rewind(file);
-                rio_writen(connfd, &file_size, sizeof(long));
-
-                while ((bytes_read = Fread(filebuf, 1, MAXLINE, file)) > 0)
-                {
-                    printf("Line: %s\n", filebuf); // ICI
-
-                    printf("Server sending %ld bytes\n", bytes_read);
-                    Rio_writen(connfd, filebuf, bytes_read);
-                }
-                Fclose(file);
-                printf("File sent and closed: %s\n", filename);
-            }
-            else
-            {
-                char *msg = "File not found\n";
-                printf("File not found: %s\n", filename);
-                Rio_writen(connfd, msg, strlen(msg));
-            }
+          printf("Server sending %ld bytes\n", bytes_read);
+          Rio_writen(connfd, filebuf, bytes_read); // Send the file
         }
-        else
-        {
-            printf("Server received %u bytes\n", (unsigned int)n);
-            Rio_writen(connfd, buf, n);
-        }
+        Fclose(file);
+        printf("File sent and closed: %s\n", filename);
+      }
+      else
+      {
+        char *msg = "File not found\n";
+        printf("File not found: %s\n", filename);
+        Rio_writen(connfd, msg, strlen(msg));
+      }
     }
+    else
+    {
+      printf("Server received %u bytes\n", (unsigned int)n);
+      Rio_writen(connfd, buf, n);
+    }
+  }
 }
