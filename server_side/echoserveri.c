4c1c92d4bdf22224c437c6c703f8e4d587ee3d55
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-03-26 16:27:34 +0100

    Create the number of childs dynamically

--- server_side/echoserveri.c
+++ server_side/echoserveri.c
@@ -10,70 +10,82 @@ pid_t pid_fils[NB_PROC];
 
 void echo(int connfd);
 
-/* 
+/*
  * Note that this code only works with IPv4 addresses
  * (IPv6 is not supported)
  */
 
-void handler(int sig) {
-    for (int i = 0; i < 5; i++) {
-        printf("fils mort {%d}\n", pid_fils[i]);
-        kill(SIGINT, pid_fils[i]);
-        waitpid(-1, NULL, 0);
-    }
-    exit(0);
+void handler(int sig)
+{
+  for (int i = 0; i < 5; i++)
+  {
+    printf("fils mort {%d}\n", pid_fils[i]);
+    kill(SIGINT, pid_fils[i]);
+    waitpid(-1, NULL, 0);
+  }
+  exit(0);
 }
 
-void socket_fils(int listenfd) {
-    socklen_t clientlen;
-    struct sockaddr_in clientaddr;
-    char client_ip_string[INET_ADDRSTRLEN];
-    char client_hostname[MAX_NAME_LEN];
-    
-    while (1) {
-        clientlen = sizeof(clientaddr);
-        int connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+void socket_fils(int listenfd)
+{
+  socklen_t clientlen;
+  struct sockaddr_in clientaddr;
+  char client_ip_string[INET_ADDRSTRLEN];
+  char client_hostname[MAX_NAME_LEN];
 
-        /* determine the name of the client */
-        Getnameinfo((SA *) &clientaddr, clientlen,
-                    client_hostname, MAX_NAME_LEN, 0, 0, 0);
+  while (1)
+  {
+    clientlen = sizeof(clientaddr);
+    int connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
 
-        /* determine the textual representation of the client's IP address */
-        Inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip_string,
-                INET_ADDRSTRLEN);
+    /* determine the name of the client */
+    Getnameinfo((SA *)&clientaddr, clientlen,
+                client_hostname, MAX_NAME_LEN, 0, 0, 0);
 
-        printf("server connected to %s (%s)\n", client_hostname,
-            client_ip_string);
+    /* determine the textual representation of the client's IP address */
+    Inet_ntop(AF_INET, &clientaddr.sin_addr, client_ip_string,
+              INET_ADDRSTRLEN);
 
-        echo(connfd);
-        Close(connfd);
-    }
+    printf("server connected to %s (%s)\n", client_hostname,
+           client_ip_string);
+
+    echo(connfd);
+    Close(connfd);
+  }
 }
 
-int main() {
-    int listenfd, port;
-    pid_t fils;
-    int i = 0;
-    
-    port = 2121;
+int main()
+{
+  int listenfd, port;
+  pid_t fils;
+  int i = 0;
 
-    listenfd = Open_listenfd(port);
-    Signal(SIGINT, handler);
+  port = 2121;
 
-    while (i < 5) {
-        fils = Fork();
-        if (fils == 0) { // child process
-            Signal(SIGINT, SIG_DFL);
-            socket_fils(listenfd);
-            exit(0); // Ensure child process exits after handling connections
-        } else if (fils > 0) { // parent process
-            pid_fils[i] = fils;
-            i++;
-        } else {
-            perror("fork");
-            exit(0);
-        }
+  listenfd = Open_listenfd(port);
+  Signal(SIGINT, handler);
+
+  while (i < NB_PROC)
+  {
+    fils = Fork();
+    if (fils == 0)
+    { // child process
+      Signal(SIGINT, SIG_DFL);
+      socket_fils(listenfd);
+      exit(0); // Ensure child process exits after handling connections
     }
-    while (1);
-    exit(0);
+    else if (fils > 0)
+    { // parent process
+      pid_fils[i] = fils;
+      i++;
+    }
+    else
+    {
+      perror("fork");
+      exit(0);
+    }
+  }
+  while (1)
+    ;
+  exit(0);
 }
