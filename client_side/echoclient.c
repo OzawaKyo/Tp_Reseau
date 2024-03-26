16ff86b7116f5aaa70529a90d892a3e55f582c40
Mohamed Amarcha <simoamarcha@icloud.com>
2024-03-26 16:15:57 +0100

    patch 3

--- client_side/echoclient.c
+++ client_side/echoclient.c
@@ -1,11 +1,14 @@
 #include "csapp.h"
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int clientfd, port;
     char *host, buf[MAXLINE];
     rio_t rio;
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "usage: %s <host>\n", argv[0]);
         exit(0);
     }
@@ -15,32 +18,47 @@ int main(int argc, char **argv) {
     clientfd = Open_clientfd(host, port);
     Rio_readinitb(&rio, clientfd);
 
-    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+    // Waiting client instructions
+    while (Fgets(buf, MAXLINE, stdin) != NULL)
+    {
+
         Rio_writen(clientfd, buf, strlen(buf));
-        if (strncmp(buf, "get ", 4) == 0) {
+
+        // Wants to get a file
+        if (strncmp(buf, "get ", 4) == 0)
+        {
             char filename[MAXLINE];
             sscanf(buf + 4, "%s", filename);
             char filebuf[MAXLINE];
-            if (Rio_readlineb(&rio, buf, MAXLINE) > 0 && strncmp(buf, "File not found", 14) != 0) {
-                FILE* file = Fopen(filename, "wb");
-                if (file != NULL) {
-                    printf("Receiving file: %s\n", filename);
-                    ssize_t bytes_read;
-                    while ((bytes_read = Rio_readnb(&rio, filebuf, MAXLINE)) > 0) {
-                        printf("Received %ld bytes\n", bytes_read);
-                        Fwrite(filebuf, 1, bytes_read, file);
-                    }
+
+            long file_size;
+            Rio_readnb(&rio, &file_size, sizeof(long));
+
+            FILE *file = Fopen(filename, "wb");
+            if (file != NULL)
+            {
+                ssize_t bytes_read;
+                size_t total_bytes_read = 0;
+                while (total_bytes_read < file_size)
+                {
+                    printf("Client receiving %ld bytes\n", file_size - total_bytes_read);
+                    printf("a\n");
+                    bytes_read = Rio_readnb(&rio, filebuf, MIN(file_size - total_bytes_read, MAXLINE));
+                    printf("b\n");
+                    Fwrite(filebuf, 1, bytes_read, file);
+                    printf("c\n");
+                    total_bytes_read += bytes_read;
+                    printf("d\n");
                     Fclose(file);
                     printf("File received and saved: %s\n", filename);
-                } else {
-                    printf("Error: Failed to open file for writing.\n");
                 }
-            }else{
-                printf("Error: File not found.\n");
+            }
+            else
+            {
+                printf("Error: Failed to open file for writing.\n");
             }
         }
     }
     Close(clientfd);
     exit(0);
 }
-
